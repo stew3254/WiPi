24d3751115f32bd2c1519c9d801933e01fb4e6a1
stew3254 <rtstewy@gmail.com>
2018-08-08 09:44:48 -0400

    Add new conf files

--- setup.sh
+++ setup.sh
@@ -9,8 +9,15 @@ fi
 # Install hostpad and dnsmasq
 echo "Installing hostapd and dnsmasq"
 apt-get install hostapd dnsmasq -y &> /dev/null
-echo "Stopping services"
+
 systemctl stop hostapd
 systemctl stop dnsmasq
 
+# Create dhcpcd.conf files
+cp /etc/dhcpcd.conf /etc/dhcpcd.conf.original
+cp /etc/dhcpcd.conf /etc/dhcpcd.conf.offline
+cp /etc/dhcpcd.conf /etc/dhcpcd.conf.online
 
+
+echo -e "interface wlan0\n\tstatic ip_address=192.168.4.1/24\n\tnohook wpa_supplicant" | tee -a /etc/dhcpcd.conf
+service dhcpcd restart
